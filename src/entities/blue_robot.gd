dfdafb1930e97937c9231a6c8717c6eef30eff35
Jyotiraditya Tiwary <78427496+jyotiradityatiwary@users.noreply.github.com>
2024-06-10 17:59:20 +0530

    fix: make jumping and acceleration smoother

--- src/entities/blue_robot.gd
+++ src/entities/blue_robot.gd
@@ -1,26 +1,26 @@
 extends CharacterBody2D
 
-const MAX_SPEED: float = 1000.0
-const ACCELERATION: float = 5000.0
-const JUMP_SPEED: float = 500.0
-const MAX_JUMP_TIME: float = 0.8
-const DASH_MAX_SPEED: float = MAX_SPEED * 4
+const MAX_SPEED: float = 2000
+const ACCELERATION: float = FRICTION + 500
+const JUMP_SPEED: float = 1000.0
+const DASH_MAX_SPEED: float = 3000
 const DASH_CHARGE_RATE: float = 0.50
-const FRICTION: float = 2.0
+const FRICTION: float = 2000.0
 const RIGHT_DASH_DIRECTION: Vector2 = Vector2(4,-1)
 const LEFT_DASH_DIRECTION: Vector2 = Vector2(-4, -1)
 const MULTIPLAYER_POSITION_LERP_WEIGHT: float = 0.7
 
 const robot_type: GameManager.RobotType = GameManager.RobotType.BLUE
-	# Get the gravity from the project settings to be synced with RigidBody nodes.
+# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
-var jump_time: float = 0
+var is_jumping: bool = false
 var dash_charge: float = 0.0
 var is_locally_controlled: bool = false
 var player_name: String
 var synced_global_position: Vector2 = self.global_position
 
 func _physics_process(delta):
+	print(velocity.x)
 	# Add the gravity.
 	if not is_locally_controlled:
 		global_position = global_position.lerp(synced_global_position, \
@@ -30,9 +30,10 @@ func _physics_process(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = - JUMP_SPEED
-			jump_time += delta
+			is_jumping = true
 		else:
-			#var direction = Input.get_axis("ui_left", "ui_right")
+			is_jumping =  false
+			
 			var direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 			
 			if Input.is_action_pressed("dash"):
@@ -41,19 +42,17 @@ func _physics_process(delta):
 			elif dash_charge > 0.0:
 					velocity = dash_charge * DASH_MAX_SPEED * direction
 					dash_charge = 0
-			elif direction.x != 0:
-					#velocity.x = direction.x * SPEED
-					velocity.x = move_toward(velocity.x, MAX_SPEED * direction.x, ACCELERATION * delta)
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta * absf(velocity.x))
+			elif direction.x != 0 and absf(velocity.x) < MAX_SPEED:
+					velocity.x += ACCELERATION * delta * direction.x
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 	else:
-		dash_charge = 0
-		if jump_time > 0 and jump_time < MAX_JUMP_TIME and Input.is_action_pressed("jump"):
-			velocity.y = - JUMP_SPEED * (1 - jump_time / MAX_JUMP_TIME)
-			jump_time += delta
-			print("jump, v=", velocity.y, ", f=", jump_time/MAX_JUMP_TIME)
-		else:
-			jump_time = 0
-			velocity.y += gravity * delta
+		dash_charge = 0.0
+		if is_jumping:
+			if not (velocity.y < 0 and Input.is_action_pressed("jump")):
+				velocity.y *= 0.6 # to suddenly halt jumping without much of a visible jerk
+				is_jumping = false
+		
+		velocity.y += gravity * delta
 
 	move_and_slide()
 	synced_global_position = self.global_position
